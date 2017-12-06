bd790763ea8038954badc6d9d7cfee8dea3efcb2
Nthabi <nthabimashiane@gmail.com>
2017-12-06 14:01:12 +0200

    Did string searching for endings in extensions.txt

--- Analysis.java
+++ Analysis.java
@@ -9,6 +9,48 @@ public class Analysis{
     ArrayList<String> xhosaVerbs = readFile("xhosaRoots.txt");
 
     //get combiantions of extensions
+    rootExtCombinations();
+
+    //getVerbEndings matches
+    rootEndings(zuluVerbs);
+    rootEndings(xhosaVerbs);
+
+
+  }
+
+  public static ArrayList<String> readFile(String filename){
+    Scanner scan = new Scanner(System.in);
+    File inputFile = new File(filename);
+    ArrayList<String> verbArray = new ArrayList<String>();
+    try{
+      scan = new Scanner(inputFile);
+      while(scan.hasNextLine()){
+        verbArray.add(scan.nextLine());
+      }
+    }catch(IOException e){
+      System.out.println("IO Error");
+    }
+    return verbArray;
+  }
+
+  public static void writeToFile(String outputFile, ArrayList<String> values){
+    try{
+      FileWriter fstream = new FileWriter(outputFile,true);
+      BufferedWriter out = new BufferedWriter(fstream);
+      for(String element: values){
+        out.write(element + '\n');
+      }
+      out.close();
+    }catch(IOException e){
+      System.err.println(e.getMessage());
+    }
+
+
+  }
+
+
+  public static void rootExtCombinations(){
+    /*Takes root extensions and creates possible combinations of longer extensions*/
     Permute p = new Permute();
 
     ArrayList<String> extensionsInput = readFile("extensions.txt");
@@ -34,27 +76,49 @@ public class Analysis{
       System.err.println("Error while writing to file: " +
              e.getMessage());
     }
+
   }
 
-  public static ArrayList<String> readFile(String filename){
-    Scanner scan = new Scanner(System.in);
-    File inputFile = new File(filename);
-    ArrayList<String> verbArray = new ArrayList<String>();
-    try{
-      scan = new Scanner(inputFile);
-      while(scan.hasNextLine()){
-        verbArray.add(scan.nextLine());
+  public static void rootEndings(ArrayList<String> verbRoots){
+    //read in extensions.txt, check endings, keep count and write to file
+    ArrayList<String> verbExtensions = readFile("extensions.txt");
+    ArrayList<String> matchingVerbRoots = new ArrayList<String>(); //array to keep matching roots
+    int wordCount = 0;
+
+    for(String verbRoot: verbRoots){
+      if(verbRoot.length() > 2){
+        for(int i=0; i<verbExtensions.size(); i++){
+          String extension = verbExtensions.get(i);
+          String verbEnding = verbRoot.substring(verbRoot.length()-2);
+          if(extension.length() == 2){
+            if(verbEnding.equals(extension)){
+              wordCount++;
+              matchingVerbRoots.add(verbRoot);
+              System.out.println(extension + " " + verbRoot);
+            }
+          }
+          //
+          //String verbEnding = verbRoot.substring(verbRoot.length()-2);
+          if(extension.length() == 1){
+            verbEnding = verbRoot.substring(verbRoot.length()-1);
+            if(verbEnding.equals(extension)){
+              wordCount++;
+              matchingVerbRoots.add(verbRoot);
+              System.out.println(extension + " " + verbRoot);
+            }
+          }
+          else{
+            if(verbEnding.equals(extension)){
+              wordCount++;
+              matchingVerbRoots.add(verbRoot);
+              System.out.println(extension + " " + verbRoot);
+            }
+          }
+        }
+
       }
-    }catch(IOException e){
-      System.out.println("IO Error");
     }
-    return verbArray;
   }
 
-
-  public static void rootExtCombinations(){
-    /*Takes root extensions and creates possible combinations of longer extensions*/
-    ArrayList<String> extensions = readFile("extensions.txt");
-
-  }
+  
 }
